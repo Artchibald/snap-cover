b6c2fb3c7f4fa7c9373f9ad608bc3d8843526e71
Archibald Butler <archibaldbutler@archie.local>
2024-08-12 22:47:54 +0100

    started

--- snap-cover-app/src/assets/js/footer.tsx
+++ snap-cover-app/src/assets/js/footer.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
-import { Box, Typography, Link } from '@mui/material';
+import { Box, Typography, Link, Grid } from '@mui/material';
 import styles from './footer.module.scss';
-import logo from '../img/snap-cover-logo-white.svg'
+import logo from '../img/snap-cover-logo-white.svg';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import GavelIcon from '@mui/icons-material/Gavel';
+import PolicyIcon from '@mui/icons-material/Policy';
+import CopyrightIcon from '@mui/icons-material/Copyright';
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+import HomeIcon from '@mui/icons-material/Home';
+import EmailIcon from '@mui/icons-material/Email';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import PhoneIcon from '@mui/icons-material/Phone';
+import Twitter from '../img/twitter-icon.png';
+import Telegram from '../img/telegram-icon.png';
+import Metamask from '../img/metamask-icon.png';
+
 const Footer: React.FC = () => {
     return (
         <>
@@ -14,31 +27,67 @@ const Footer: React.FC = () => {
                             <span>COVER</span>
                         </Typography>
                         <Typography variant="body1">
-                            22 George street, W2 5DT, Strand, London, UK
+                            <LocationOnIcon sx={{ marginRight: '8px', color: 'gray', top: '8px', position: "relative" }} />
+                            <Link href="22 George street, W2 5DT, Strand, London, UK"> 22 George street, W2 5DT, Strand, London, UK</Link>
                         </Typography>
                         <Typography variant="body1">
-                            info@snapcover.com
+                            <EmailIcon sx={{ marginRight: '8px', color: 'gray', top: '8px', position: "relative" }} />
+                            <Link href="mailto:info@snapcover.com">info@snapcover.com</Link>
                         </Typography>
                         <Typography variant="body1">
-                            +44(0)208 932 2343
+                            <PhoneIcon sx={{ marginRight: '8px', color: 'gray', top: '8px', position: "relative" }} />
+                            <Link href="callto:00442089322343">+44(0)208 932 2343</Link>
                         </Typography>
                         <Typography variant="h6" component="h6" className={styles.connect}>
                             CONNECT WITH US
                         </Typography>
+                        <Link href="https://x.com/red_pulse_china?lang=en"><img src={Twitter} alt="Twitter Logo" className={styles.socialIcon} /></Link>
+                        <Link href="tg://resolve?domain=username"> <img src={Telegram} alt="Telegram Logo" className={styles.socialIcon} /></Link>
+                        <Link href="https://metamask.io/snaps/">  <img src={Metamask} alt="Metmask Snaps Logo" className={styles.socialIcon} /></Link>
                     </Box>
+
                     <Box className={styles.footerColumn}>
-                        <Typography variant="h6" className={styles.footerTitle}>
-                            Quick Links
-                        </Typography>
-                        <Link href="#" className={styles.footerLink}>Terms of Service</Link><br />
-                        <Link href="#" className={styles.footerLink}>Privacy Policy</Link><br />
-                        <Link href="#" className={styles.footerLink}>Contact Us</Link>
+                        <Grid container spacing={4} className={styles.footerGrid}>
+                            <Grid item xs={12} md={6}>
+                                <Typography variant="h6" component="h6" className={styles.connect}>
+                                    BRINGING PEACE OF MIND
+                                </Typography>
+                                <Typography variant="body1">
+                                    <GavelIcon sx={{ marginRight: '8px', color: 'gray', top: '8px', position: 'relative' }} />
+                                    <Link href="#">TERMS</Link>
+                                </Typography>
+                                <Typography variant="body1">
+                                    <PolicyIcon sx={{ marginRight: '8px', color: 'gray', top: '8px', position: 'relative' }} />
+                                    <Link href="#">CONDITIONS</Link>
+                                </Typography>
+                                <Typography variant="body1">
+                                    <CopyrightIcon sx={{ marginRight: '8px', color: 'gray', top: '8px', position: 'relative' }} />
+                                    <Link href="#">PRIVACY</Link>
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <Typography variant="h6" component="h6" className={styles.connect}>
+                                    NAVIGATE
+                                </Typography>
+                                <Typography variant="body1">
+                                    <HomeIcon sx={{ marginRight: '8px', color: 'gray', top: '8px', position: 'relative' }} />
+                                    <Link href="/" target="_blank">HOME</Link>
+                                </Typography>
+                                <Typography variant="body1">
+                                    <PsychologyIcon sx={{ marginRight: '8px', color: 'gray', top: '8px', position: 'relative' }} />
+                                    <Link href="https://t.me/snapcover" target="_blank">HOW IT WORKS</Link>
+                                </Typography>
+                                <Typography variant="body1">
+                                    <SmartphoneIcon sx={{ marginRight: '8px', color: 'gray', top: '8px', position: 'relative' }} />
+                                    <Link href="https://metamask.io/" target="_blank">DAPP</Link>
+                                </Typography>
+                            </Grid>
+                        </Grid>
                     </Box>
                 </Box>
-
             </Box>
             <Typography variant="body2" className={styles.footerBottom}>
-                2024 - SNAP COVER - All rights reserved, see terms and conditions.
+                2024 - SNAP COVER - All rights reserved, see  <Link href="#" target="_blank">terms</Link> and  <Link href="#" target="_blank">conditions</Link>.
             </Typography>
         </>
     );
